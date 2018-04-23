e66df61103374063c8cae9694b2220dc70a97d15
Ryan Hübert <rhubert@ucdavis.edu>
2018-04-22 17:23:19 -0700

    Remove deprecated command

--- judges/__init__.py
+++ judges/__init__.py
@@ -11,7 +11,6 @@ import os
 import sys
 current_path = os.path.dirname(os.path.abspath( __file__ ))
 sys.path.append(current_path)
-#os.chdir(current_path) # deprecated
 
 from judges import LoadData
 from judges import QueryTools
